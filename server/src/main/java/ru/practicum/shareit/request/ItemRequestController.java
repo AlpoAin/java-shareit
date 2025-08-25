7e8b6c60c33728563051749c0f6e271effbfa55b
AlpoAin <a.sabyrgaliyev@yandex.kz>
2025-08-25 16:36:16 +0500

    feat(server): implement item requests endpoints

--- server/src/main/java/ru/practicum/shareit/request/ItemRequestController.java
+++ server/src/main/java/ru/practicum/shareit/request/ItemRequestController.java
@@ -1,12 +1,42 @@
 package ru.practicum.shareit.request;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.request.dto.ItemRequestDto;
 
-/**
- * TODO Sprint add-item-requests.
- */
+import java.util.List;
+
+import static ru.practicum.shareit.common.HeaderNames.X_SHARER_USER_ID;
+
 @RestController
-@RequestMapping(path = "/requests")
+@RequestMapping("/requests")
+@RequiredArgsConstructor
+@Validated
 public class ItemRequestController {
+    private final ItemRequestService service;
+
+    @PostMapping
+    public ItemRequestDto create(@RequestHeader(X_SHARER_USER_ID) Long userId,
+                                 @RequestBody ItemRequestDto dto) {
+        return service.create(userId, dto);
+    }
+
+    @GetMapping
+    public List<ItemRequestDto> getOwn(@RequestHeader(X_SHARER_USER_ID) Long userId) {
+        return service.getOwn(userId);
+    }
+
+    @GetMapping("/all")
+    public List<ItemRequestDto> getAll(@RequestHeader(X_SHARER_USER_ID) Long userId,
+                                       @RequestParam(defaultValue = "0") Integer from,
+                                       @RequestParam(defaultValue = "10") Integer size) {
+        return service.getAll(userId, from, size);
+    }
+
+    @GetMapping("/{requestId}")
+    public ItemRequestDto get(@RequestHeader(X_SHARER_USER_ID) Long userId,
+                              @PathVariable Long requestId) {
+        return service.get(userId, requestId);
+    }
 }
