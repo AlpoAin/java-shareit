15604b7e2c8abe98bd6c4eaec17a8251b04d3fec
AlpoAin <a.sabyrgaliyev@yandex.kz>
2025-08-26 16:59:46 +0500

    checkstyle

--- gateway/src/main/java/ru/practicum/shareit/user/UserDto.java
+++ gateway/src/main/java/ru/practicum/shareit/user/UserDto.java
@@ -4,7 +4,10 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
-@Data @NoArgsConstructor @AllArgsConstructor @Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class UserDto {
     @NotBlank
     private String name;
