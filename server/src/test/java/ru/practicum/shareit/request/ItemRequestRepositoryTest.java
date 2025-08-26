126b3da321556c58fc31606a871e7bfe82b25fcd
AlpoAin <a.sabyrgaliyev@yandex.kz>
2025-08-26 13:27:41 +0500

    test(server): fix ItemRequestRepositoryTest by seeding user to satisfy FK

--- server/src/test/java/ru/practicum/shareit/request/ItemRequestRepositoryTest.java
+++ server/src/test/java/ru/practicum/shareit/request/ItemRequestRepositoryTest.java
@@ -3,6 +3,7 @@ package ru.practicum.shareit.request;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
 import ru.practicum.shareit.request.model.ItemRequest;
 import ru.practicum.shareit.request.storage.CrudItemRequestJpaRepository;
 
@@ -11,8 +12,11 @@ import java.time.LocalDateTime;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
+@Sql(statements = "INSERT INTO users(id, name, email) VALUES (1,'test-user','test@example.com');")
 class ItemRequestRepositoryTest {
-    @Autowired CrudItemRequestJpaRepository repo;
+
+    @Autowired
+    CrudItemRequestJpaRepository repo;
 
     @Test
     void saveAndFind() {
@@ -21,6 +25,7 @@ class ItemRequestRepositoryTest {
                 .requestorId(1L)
                 .created(LocalDateTime.now())
                 .build());
+
         assertThat(repo.findById(r.getId())).isPresent();
     }
 }
